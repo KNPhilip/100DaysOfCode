7fa6fd91bc63084603030dc56ad52af7682d4e91
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-05-05 15:15:23 +0200

    Make 36th Blazor project

---
+++ Blazor/36-HeadOutlet/HeadOutlet/Program.cs
@@ -0,0 +1,17 @@
+using HeadOutlet.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
