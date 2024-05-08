8e96ea054b965395b7b2bea6f03ecf833820a456
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-05-08 19:29:31 +0200

    Make 39th Blazor project

---
+++ Blazor/39-EmailSender/EmailSender/Program.cs
@@ -0,0 +1,19 @@
+using EmailSender.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
