32d39bb0c0bdf6c0fcc0407386e9372bb45ecffa
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-05-13 15:22:56 +0200

    Make 44th Blazor project

---
+++ Blazor/44-ToDo/ToDoApp/Program.cs
@@ -0,0 +1,19 @@
+using ToDoApp.Components;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
