5d7c9c4a3f8c8c1bc4ba87a8925ac00b584ea095
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-06-09 19:03:24 +0200

    Make 71st Blazor project

---
+++ Blazor/71-PetAdoptionPortal/PetAdoptionPortal/Models/Dog.cs
@@ -0,0 +1,9 @@
+﻿namespace PetAdoptionPortal.Models;
+
+public sealed class Dog
+{
+    public string? Name { get; set; }
+    public string? Breed { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+}
