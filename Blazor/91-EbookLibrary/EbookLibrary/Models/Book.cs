55e190d6f731a69e7939459768368506dcf1626b
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-06-29 18:48:51 +0200

    Make 91st Blazor project

---
+++ Blazor/91-EbookLibrary/EbookLibrary/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace EbookLibrary.Models;
+
+public sealed class Book
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Author { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string SourceUrl { get; set; }
+    public required decimal Price { get; set; }
+}
