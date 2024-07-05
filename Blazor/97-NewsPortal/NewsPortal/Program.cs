dcfa718525ff7ee10a1e043f61e4e3d006397b16
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2024-07-05 18:49:50 +0200

    Make 97th Blazor project

---
+++ Blazor/97-NewsPortal/NewsPortal/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NewsPortal;
+
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
+
+await builder.Build().RunAsync();
